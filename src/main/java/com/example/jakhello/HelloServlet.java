74b396f37453520fefa7bc187554670c4c9d7c1c
anaperez00 <aperezdi@unbosque.edu.co>
2021-10-05 02:21:37 -0500

    desplegado en el servidor de aplicaciones

---
+++ src/main/java/com/example/jakhello/HelloServlet.java
@@ -0,0 +1,41 @@
+package com.example.jakhello;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+// a travez de clases que extienden de httpservlet
+//tiene un ciclo de vida
+// init algo parecido a un metodo constructor
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class  HelloServlet extends HttpServlet {
+    private String message;
+
+    //metodo constructor
+    public void init() {
+        message = "Hello World!";
+    }
+
+    // puedo hacer llamados por medio de una url y un puerto  utilizando diferentes metodos que vienen con el estandar http
+    // pueden ser get, put, poust, y delete estos son los principlaes
+    //get: traer informacion del servidor
+    // el post: cuando quiero enviar informacion pero que esa informacion es nueva
+    // el put: cuando quiero actualizar una informacion existenete
+    // delete: para eliminar
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // puedo responder un json, un text por ahora un html ya que quiero que el navegador imprima de una vez 
+        response.setContentType("text/html");
+
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    /* si despues de que finalize ese servlet quiero desatruir una variable para liberar espacion en memoria,o para guardar un log en donde
+   el usuario ya abandono la pagina  */
+    public void destroy() {
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %></h1>
+<br/>
+<!-- por detras jakarta o el servidor de aplicaciones le hace match con lo que se encuentra en esa clase de hello servlet-->
+<a href="hello-servlet">Hello Servlet</a>
+</body>
+</html>
