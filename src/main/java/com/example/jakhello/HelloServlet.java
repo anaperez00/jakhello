ccb3a1c5e19f450f31d786a5c4f9be015192ff5e
anaperez00 <aperezdi@unbosque.edu.co>
2021-10-06 02:44:44 -0500

    Extending helloWorld with POST form

--- src/main/java/com/example/jakhello/HelloServlet.java
+++ src/main/java/com/example/jakhello/HelloServlet.java
@@ -9,11 +9,11 @@ import javax.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class  HelloServlet extends HttpServlet {
-    private String message;
+    //private String message;
 
     //metodo constructor
     public void init() {
-        message = "Hello World!";
+     //   message = "Hello World!";
     }
 
     // puedo hacer llamados por medio de una url y un puerto  utilizando diferentes metodos que vienen con el estandar http
@@ -22,16 +22,32 @@ public class  HelloServlet extends HttpServlet {
     // el post: cuando quiero enviar informacion pero que esa informacion es nueva
     // el put: cuando quiero actualizar una informacion existenete
     // delete: para eliminar
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  /* public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         // puedo responder un json, un text por ahora un html ya que quiero que el navegador imprima de una vez
         response.setContentType("text/html");
-
-
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
+    }*/
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        //imprimo un log con los parametros que me acaban de llegar
+        System.out.println("username: "+username);
+        System.out.println("password: "+password);
+
+        if(username.equals("aperezdi")&& password.equals("1234")){
+            response.setContentType("text/html");
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1> Hello " + username + "</h1>");
+            out.println("</body></html>");
+        } else {
+            response.sendError(401);
+        }
     }
 
     /* si despues de que finalize ese servlet quiero desatruir una variable para liberar espacion en memoria,o para guardar un log en donde
@@ -39,3 +55,4 @@ public class  HelloServlet extends HttpServlet {
     public void destroy() {
     }
 }
+
